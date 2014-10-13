8b67d1a5822a4b5e0e1cff8d607159884e89e308
Kevin Gregory <medkg15@gmail.com>
2014-10-13 00:11:41 -0400

    Add js tree to requirejs config.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
 				modules: [
 					{
 						name: 'common',
-						include: ['jquery', 'bootstrap', 'underscore', 'knockout',]
+						include: ['jquery', 'bootstrap', 'underscore', 'knockout','jstree']
 					},
 					{
 						name: 'app/gameViewModel',
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
 				modules: [ 
 					{
 						name: 'common',
-						include: ['jquery', 'bootstrap', 'underscore', 'knockout',]
+						include: ['jquery', 'bootstrap', 'underscore', 'knockout','jstree']
 					},
 					{
 						name: 'app/gameViewModel',
