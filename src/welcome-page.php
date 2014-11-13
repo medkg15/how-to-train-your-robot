df81de16940dd59a00052e104608a1a96d4f1981
nella14 <marianellaryd@gmail.com>
2014-11-13 12:36:50 -0500

    play button

--- src/welcome-page.php
+++ src/welcome-page.php
@@ -8,7 +8,9 @@
         <div class="col-md-3 home-sidebar">
             <img src="/images/logo.png" alt="Robot Training" style="width:100%;">
 
-            <button class="btn-lg btn-block" data-bind="click: startGame">Play!</button>
+            <button class="btn-lg btn-block" data-bind="click: startGame">Play!
+                <img src="/images/playButton.png" alt="Play button" style="width:100%;">
+            </button>
 
             <button class="btn-lg btn-block">Customize</button>
 
