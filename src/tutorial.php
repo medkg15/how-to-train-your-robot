8631021dfad10e75f2c7fbb6c93e216c7dfb5686
nella14 <marianellaryd@gmail.com>
2014-11-15 20:24:27 -0500

    add tutorial.php file and two images level1tutorialPart1 and level1tutorialPart2

--- src/tutorial.php
+++ src/tutorial.php
@@ -2,8 +2,8 @@
 
     <div class="well">
         <h1>The Tutorial</h1>
-        <br><img src="images/level1tutorialPart1.png" width="800" height="600"/>
-        <br><img src="images/level1tutorialPart2.png" width="800" height="600"/>
+        <br><img src="/images/level1tutorialPart1.png" alt="level1 Tutorial Part1" style="width:100%;" width="800" height="600"/>
+        <br><img src="/images/level1tutorialPart2.png" alt="level1 Tutorial Part2" style="width:100%;"width="800" height="600"/>
 
     </div>
 </div>
