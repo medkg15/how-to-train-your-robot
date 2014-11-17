4837b2c2d35f741f5c6cfdc96ee528df8f2a9d01
medkg15 <medkg15@gmail.com>
2014-11-17 12:53:55 -0500

    high scores loading bar

--- src/high-scores.php
+++ src/high-scores.php
@@ -3,7 +3,11 @@
     <div class="well">
         <h1>High Scores</h1>
         <div data-bind="if: loadingHighScores">
-            <p>Loading...</p>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
         </div>
         <table class="table table-border" data-bind="if: !loadingHighScores()">
             <thead>
