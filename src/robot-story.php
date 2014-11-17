8dd7ca3b028479ef3a1efa5ea959014da24cb9fd
Kevin Gregory <medkg15@gmail.com>
2014-11-17 15:34:41 -0500

    fix merge conflict.

--- src/robot-story.php
+++ src/robot-story.php
@@ -1,30 +1,21 @@
 <div data-bind="if: currentView() === 'robot-story'">
     <div class="well">
         <div class="row">
-            <div class="col-md-8" >
+            <div class="col-md-8">
                 <h1> Welcome to Robot Training</h1>
 
                 <p>
-<<<<<<< HEAD
-                   One day Robo was discovering the world with his spaceship...<br>Unfortunately, the spaceship had exploded in the space and tear apart in many lands<br><br>In this game you will be introduced to different programming concepts as you help Robo collect the parts of his spaceship from the different environments
-                </p>
-                <p><img src="pics/explosion.png" width="1100" height="600"/></p>
-=======
-                One day Robo was discovering the world by his spaceship. <br> Unfortunately, the spaceship had exploded in the space and tear apart in many lands. In this game you will be introduced to different programming concepts as you help Robo collect the parts of his spaceship from the different environments.
-                <img src="images/introductionBackground.jpg" width=780" height="450"/>
+                    One day Robo was discovering the world by his spaceship. <br> Unfortunately, the spaceship had
+                    exploded in the space and tear apart in many lands. In this game you will be introduced to different
+                    programming concepts as you help Robo collect the parts of his spaceship from the different
+                    environments.
+                    <img src="images/introductionBackground.jpg" width=780" height="450"/>
 
                 </p>
-
->>>>>>> origin/master
             </div>
 
             <div class="col-md-4">
-<<<<<<< HEAD
-
-                <br><br><br><br><br><br><br><br><p><img src="pics/Robot_Character1_Blue.png" width="250" height="287"/></p>
-=======
                 <p><br> <br><img src="pics/Robot_Character1_Blue.png" width="230" height="287"/></p>
->>>>>>> origin/master
 
                 <p><br> <br><img src="pics/Robot_Character1_Pink.png" width="250" height="290"/></p>
             </div>
