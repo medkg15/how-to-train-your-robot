dd4190e3d4c72cfc9a2475f8872a59a97f3dd95c
Emmy-a <emtethala@my.ccsu.edu>
2014-11-17 14:27:43 -0500

    Add the story picture for robot-story and edit the text

--- src/robot-story.php
+++ src/robot-story.php
@@ -5,11 +5,14 @@
                 <h1> Welcome to Robot Training</h1>
 
                 <p>
-                One day Robo was discovering the world by his spaceship. Unfortunately, the spaceship had exploded in the space and tear apart in many lands. In this game you will be introduced to different programming concepts as you help Robo collect the parts of his spaceship from the different environments.
+                   One day Robo was discovering the world with his spaceship...<br>Unfortunately, the spaceship had exploded in the space and tear apart in many lands<br><br>In this game you will be introduced to different programming concepts as you help Robo collect the parts of his spaceship from the different environments
                 </p>
+                <p><img src="pics/explosion.png" width="1100" height="600"/></p>
             </div>
+
             <div class="col-md-4">
-                <p><img src="pics/Robot_Character1_Blue.png" width="230" height="287"/></p>
+
+                <br><br><br><br><br><br><br><br><p><img src="pics/Robot_Character1_Blue.png" width="250" height="287"/></p>
 
                 <p><img src="pics/Robot_Character1_Pink.png" width="250" height="290"/></p>
             </div>
