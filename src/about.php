91450e8da5f556aa53e89d2c19ffccc624937403
aedaleyjr <antony.daley@gmail.com>
2014-11-17 18:59:36 -0500

    about.php
    left alignment

--- src/about.php
+++ src/about.php
@@ -17,19 +17,20 @@
 
           </div>
             <div class="col-md-4">
-                <img src="pics/ChadSmall.jpg" width="256" height="192" class="img-thumbnail"/><br/>
+                <div align="left"><img src="pics/ChadSmall.jpg" width="256" height="192" class="img-thumbnail"/><br/>
 
                 Dr. Chad Williams<br/>
                 Assistant Professor of Computer Science
+                </div>
             </div>
-        </div>
+      </div>
         <div class="row">
             <div class="col-md-8">
                 <p>&nbsp;</p>
 
           </div>
             <div class="col-md-4">
-                <p><img src="pics/no_photo_female.jpg" width="192" height="201" class="img-thumbnail"/><br/>
+                <p align="left"><img src="pics/no_photo_female.jpg" width="192" height="201" class="img-thumbnail"/><br/>
                 Emtethal Alafghani<br/>
             Computer Information Technology Masters Candidate<br />
             Concentration: Networking <br />
@@ -42,33 +43,36 @@
 
           </div>
             <div class="col-md-4">
-                <img src="pics/shotAJ.jpg" alt="" width="175" height="210" class="img-thumbnail"/><br/>
+                <div align="left"><img src="pics/shotAJ.jpg" alt="" width="175" height="210" class="img-thumbnail"/><br/>
 
                 Antony E. Daley, Jr.<br/>
                 Computer Information Technology Masters Candidate<br />
 Concentration: Networking </div>
-        </div>
+          </div>
+      </div>
         <div class="row">
             <div class="col-md-8">
                 <p>&nbsp;</p>
 
           </div>
             <div class="col-md-4">
-                <img src="pics/kevin.gif" width="200" height="200" class="img-thumbnail"/><br/>
+                <div align="left"><img src="pics/kevin.gif" width="200" height="200" class="img-thumbnail"/><br/>
                 Kevin Gregory<br/>
                 Computer Information Technology Masters Candidate<br />
 Concentration: Computer Science </div>
-        </div>
+          </div>
+      </div>
         <div class="row">
             <div class="col-md-8">
                 <p>&nbsp;</p>
 
           </div>
             <div class="col-md-4">
-                <img src="pics/marianella.gif" width="206" height="211" class="img-thumbnail"/><br/>
+                <div align="left"><img src="pics/marianella.gif" width="206" height="211" class="img-thumbnail"/><br/>
                 Marianella Rydzewski<br/>
                 Computer Information Technology Masters Candidate<br />
 Concentration: Networking </div>
-        </div>
+          </div>
+      </div>
     </div>
 </div>
