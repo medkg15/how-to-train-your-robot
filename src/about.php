d76b4feece7ddea64e175bcb2a535cc66622f150
Kevin Gregory <medkg15@gmail.com>
2014-11-20 21:48:08 -0500

    reformat about page and fix images.

--- src/about.php
+++ src/about.php
@@ -1,62 +1,84 @@
 <div class="page-container" data-bind="if: currentView() === 'about'">
 
-    <div class="well">
+    <div class="well team">
         <h1>The Team Goals</h1>
 
-        <p>The goal of this project is to accomplish the following: <br>
-           <br> 1.	Motivate young children between 8 to 10 years old to learn the fundamental concepts of computing programming such as program, automation, testing, loops, and decision making (if/then) through creating a game application.
-           <br> 2.	 Understand the fundamental concepts that are needed for teaching a student to understand programming.
-           <br> 3.	Evaluate what tools and technologies are currently available for children to learn the basics of programming.
-           <br> 4.	Leverage what has already been done in this field and develop a game-based application that will teach students (in particular elementary school students) the basics of programming.
-           <br> 5.	Pilot this game-based application to a group of children and observe and record their responsiveness to the application.  Additionally, examine whether or not participants show evidence of understanding specific programming concepts (automation, repetition, sub-routines, etc.)
-            </p>
+        <p>The goal of this project is to accomplish the following:</p>
+        <ol>
+            <li>Motivate children between 8 to 10 years old to learn the fundamental concepts of computing
+                programming such as program, automation, testing, loops, and decision making (if/then) through creating
+                a
+                game application.
+            </li>
+            <li>Understand the fundamental concepts that are needed for teaching a student to understand
+                programming.
+            </li>
+            <li>Evaluate what tools and technologies are currently available for children to learn the basics of
+                programming.
+            </li>
+            <li>Leverage what has already been done in this field and develop a game-based application that will
+                teach students (in particular elementary school students) the basics of programming.
+            </li>
+            <li>Pilot this game-based application to a group of children and observe and record their responsiveness
+                to the application. Additionally, examine whether or not participants show evidence of understanding
+                specific programming concepts (automation, repetition, sub-routines, etc.)
+            </li>
+        </ol>
+
         <h1>Project Team</h1>
-        <div class="row" align="left">
-            <div class="col-md-4">
-                <div align="left"><img src="pics/ChadSmall.jpg" width="256" height="192" class="img-thumbnail"/><br/>
 
-                Dr. Chad Williams<br/>
-                Assistant Professor of Computer Science/Advisor
-                </div>
-            </div>
-      </div>
-        <br>
-        <div class="row" align="left">
-            <div class="col-md-4" align="left">
-                <p align="left"><img src="pics/no_photo_female.jpg" width="192" height="201" class="img-thumbnail"/><br/>
-                Emtethal Alafghani<br/>
-            Computer Information Technology Masters Candidate<br />
-            Concentration: Networking <br />
-              </p>
-            </div>
-      </div>
-        <br>
         <div class="row">
-            <div class="col-md-4" align="left">
-                <div align="left"><img src="pics/shotAJ.jpg" alt="" width="175" height="210" class="img-thumbnail"/><br/>
+            <div class="col-md-6">
+                <p><img src="pics/ChadSmall.jpg" class=" pull-left img-thumbnail"/></p>
 
-                Antony E. Daley, Jr.<br/>
-                Computer Information Technology Masters Candidate<br />
-Concentration: Networking </div>
-          </div>
-      </div>
-        <br>
+                <p>
+                    <strong>Dr. Chad Williams</strong><br/>
+                    Assistant Professor of Computer Science/Advisor
+                </p>
+            </div>
+
+        </div>
         <div class="row">
-            <div class="col-md-4" align="left">
-                <div align="left"><img src="pics/kevin.gif" width="200" height="200" class="img-thumbnail"/><br/>
-                Kevin Gregory<br/>
-                Computer Information Technology Masters Candidate<br />
-Concentration: Computer Science </div>
-          </div>
-      </div>
-        <br>
+            <div class="col-md-6">
+                <p><img src="pics/no_photo_female.jpg"
+                        class="img-thumbnail pull-left"/></p>
+
+                <p><strong>Emtethal Alafghani</strong><br/>
+                    Computer Information Technology Masters Candidate<br/>
+                    Concentration: Networking <br/>
+                </p>
+            </div>
+            <div class="col-md-6">
+
+                <p><img src="pics/shotAJ.jpg" alt=""
+                        class="img-thumbnail pull-left"/></p>
+
+                <p>
+                    <strong>Antony E. Daley, Jr.</strong><br/>
+                    Computer Information Technology Masters Candidate<br/>
+                    Concentration: Networking
+                </p>
+            </div>
+
+        </div>
         <div class="row">
-            <div class="col-md-4" align="left">
-                <div align="left"><img src="pics/marianella.gif" width="206" height="211" class="img-thumbnail"/><br/>
-                Marianella Rydzewski<br/>
-                Computer Information Technology Masters Candidate<br />
-Concentration: Computer Science </div>
-          </div>
-      </div>
+
+            <div class="col-md-6">
+                <p><img src="pics/kevin.gif" class=" pull-left img-thumbnail"/></p>
+
+                <p><strong>Kevin Gregory</strong><br/>
+                    Computer Information Technology Masters Candidate<br/>
+                    Concentration: Computer Science
+                </p>
+            </div>
+            <div class="col-md-6">
+                <p><img src="pics/marianella.jpg" class=" pull-left img-thumbnail"/></p>
+
+                <p><strong>Marianella Rydzewski</strong><br/>
+                    Computer Information Technology Masters Candidate<br/>
+                    Concentration: Computer Science
+                </p>
+            </div>
+        </div>
     </div>
 </div>
