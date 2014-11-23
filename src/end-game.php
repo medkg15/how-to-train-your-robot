bc9d0efedb6fac82577e0354d61c1813cee6b4e2
nella14 <marianellaryd@gmail.com>
2014-11-23 18:07:16 -0500

    bgEndGame.png, end-game.php  and gameViewModel.js update

--- src/end-game.php
+++ src/end-game.php
@@ -5,10 +5,10 @@
 
         <div style="position:relative;">
             <img src="/images/Persona.png" alt="Persona"
-                 data-bind="introPersona: introStoryText" style="position:absolute; top:50px; left:10px;"/>
-            <img src="/images/games.gif" style="height:200px; position:absolute;left:100px; bottom:0;"/>
+                 data-bind="introPersona: endStoryText" style="position:absolute; top:50px; left:10px;"/>
+            <img src="/images/games.gif" style="position:absolute; left:850px; top:100px;"/>
 
-            <img class="img-thumbnail" src="images/explosion.png"/>
+            <img class="img-thumbnail" src="images/bgEndGame.png"/>
 
         </div>
     </div>
