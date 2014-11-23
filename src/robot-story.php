b93b6aa3f1165080003c16e1befaa701cfaaf54f
nella14 <marianellaryd@gmail.com>
2014-11-23 17:16:51 -0500

    game.gif, end game and robo-story update

--- src/robot-story.php
+++ src/robot-story.php
@@ -5,7 +5,7 @@
         <div style="position:relative;">
             <img src="/images/Persona.png" alt="Persona"
                  data-bind="introPersona: introStoryText" style="position:absolute; top:50px; left:10px;"/>
-            <img src="/images/starving-stunned-dizzy1.gif" style="position:absolute; top:50px; height:200px; left:100px; "/>
+            <img src="/images/starving-stunned-dizzy1.gif" style="position:absolute; left:400px; "/>
 
             <img class="img-thumbnail" src="images/explosion.png"/>
 
